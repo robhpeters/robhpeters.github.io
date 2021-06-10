cbc47dbb8bb8e3ad2f949f69a7d9140a2c546f09
robhpeters <85690234+robhpeters@users.noreply.github.com>
2021-06-10 17:51:02 -0400

    Update hw5.js

--- js/hw5.js
+++ js/hw5.js
@@ -1,7 +1,6 @@
-
-
 (function() {
     var httpRequest;
+    // get the JSON data file from the server location
     document.getElementById("myButton").onclick = function() { getJSONData('https://robhpeters.github.io/data/my_degrees.json');
   };
   // getJSONData creates a new XMLHttpRequest object
@@ -12,7 +11,7 @@
        alert("Exiting : Cannot create an XMLHTTP instance");
        return false;
      }
-     // handle the server response, call alerContents when the state of the request changes
+     // handle the server response, call printContents when the state of the request changes
      httpRequest.onreadystatechange = printContents;
      // make the request with open() and send()
      httpRequest.open('GET', url);
@@ -23,20 +22,26 @@
     if (httpRequest.readyState === XMLHttpRequest.DONE) {
       // check the response code, 200 indicates a successful AJAX call
       if (httpRequest.status === 200) {
-        // if successful, write the responseText property of the XMLHttpRequest object
+        // if successful, parse the contents of responseText
         const myObj = JSON.parse(this.responseText);
+        // start an HTML table
         let text = "<table border='1'>"
         var i;
+        // loop through the my_degrees object
         for (i = 0; i < myObj['my_degrees'].length; i++) {
+          // for each degree object in my_degrees write a new table row
           const degreeObj = myObj['my_degrees'][i]['degree'];
           text += "<tr>"
-          // Object.values(animals).forEach(val => console.log(val));
+          // for each property in the degree Object write the table data
           for (const property in degreeObj){
             text += "<td>" + degreeObj[property] + "</td>";
           }
+          // close the table row
           text += "</tr>";
         }
+        // close the table
         text += "</table>"
+        // write the table out via DOM jsondata element
         document.getElementById("jsondata").innerHTML = text;
       } else {
         // if our AJAX call is unsuccessful, provide an alert indicating that there was a problem
