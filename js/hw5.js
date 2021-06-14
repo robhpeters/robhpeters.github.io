cc0812b7dc395f4be863c27f5e553e13d0be8c6e
robhpeters <85690234+robhpeters@users.noreply.github.com>
2021-06-14 13:16:36 -0400

    Update hw5.js

--- js/hw5.js
+++ js/hw5.js
@@ -4,7 +4,7 @@
     var httpRequest;
     // get the JSON data file from the server location
     document.getElementById("myButton").onclick = function() { getJSONData('https://robhpeters.github.io/data/my_degrees.json');
-    document.getElementById("myButton").disabled = true;
+    document.getElementById("myButton").remove();
   };
   // getJSONData creates a new XMLHttpRequest object
   function getJSONData(url) {
